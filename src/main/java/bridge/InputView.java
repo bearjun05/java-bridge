49ef873e017405d56ab0b5a403b89c4e8737e458
bearjun05 <bearjun05@naver.com>
2022-11-25 19:07:01 +0900

    feat : 게임 view, 요청, 응답 값 추가 및 상수 처리

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,28 +1,31 @@
 package bridge;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
+    public static final String QUIT = "Q";
 
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        return Integer.parseInt(readLine());
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        return readLine();
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        return readLine();
     }
 }
--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,23 +1,43 @@
 package bridge;
 
+import java.util.Stack;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
 
-    /**
-     * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void printMap() {
+    public static final String GAME_START = "다리 건너기 게임을 시작합니다."
+            + "\n다리의 길이를 입력해주세요.";
+    public static final String SELECT_MOVING_SPACE = "\n이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    public static final String RETRY_OR_FINISH = "게임을 다시 시도할지 여부를 입력해주세요. 재시도: R, 종료: Q";
+    public static final String FINAL_RESULT = "\n최종 게임 결과";
+    public static final String IS_SUCCESS = "\n게임 성공 여부: ";
+    public static final String TOTAL_ATTEMPTING_CNT = "총 시도한 횟수: ";
+
+    public void printMap(Stack<String> upstairs, Stack<String> downstairs) {
+        for (String upStair : upstairs) {
+            System.out.print(upStair);
+        }
+        System.out.println();
+        for (String downStair : downstairs) {
+            System.out.print(downStair);
+        }
     }
 
-    /**
-     * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void printResult() {
+    public void printResult(boolean isSuccess, int totalAttemptingCnt) {
+        String success = "성공";
+        if(isSuccess == true) {
+            success = "성공";
+        }
+        if(isSuccess == false) {
+            success = "실패";
+        }
+        printMessage("\n" + IS_SUCCESS + success);
+        printMessage(TOTAL_ATTEMPTING_CNT + totalAttemptingCnt);
+    }
+
+    public void printMessage(String message) {
+        System.out.println(message);
     }
 }
